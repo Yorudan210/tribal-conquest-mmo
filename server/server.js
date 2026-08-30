9253f3c0a5b134857b6668836366922fa738d62b
Yorudan210 <119311559+Yorudan210@users.noreply.github.com>
2026-08-30 14:11:11 +0200

    aserver.js : sert les fichiers .jsx avec le bon type MIME (React sans build)

--- server/server.js
+++ server/server.js
@@ -75,6 +75,7 @@ async function startServer(){
 
 const MIME = {
   ".html":"text/html; charset=utf-8", ".js":"application/javascript; charset=utf-8",
+  ".jsx":"application/javascript; charset=utf-8",
   ".css":"text/css; charset=utf-8", ".json":"application/json; charset=utf-8",
   ".svg":"image/svg+xml", ".ico":"image/x-icon",
   ".jpg":"image/jpeg", ".jpeg":"image/jpeg", ".png":"image/png", ".webp":"image/webp"
